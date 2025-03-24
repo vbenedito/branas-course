83ddf21d62b02d06e61995b9eb49cae1b99cdcd2
Vinicius Benedito <beneditoloura@gmail.com>
2025-03-24 06:34:59 -0300

    refactor: update tests and code

--- src/signup.ts
+++ src/signup.ts
@@ -1,70 +1,130 @@
 import crypto from "crypto";
 import pgp from "pg-promise";
-import express from "express";
+import express, { Response } from "express";
 import { validateCpf } from "./validateCpf";
 
 const app = express();
 app.use(express.json());
 
+function returnErrorStatusWithMessage(res: Response, message: number) {
+  res.status(422).json({ message });
+}
+
+function userAlreadyExists(account: any, res: Response) {
+  if (account) {
+    returnErrorStatusWithMessage(res, -4);
+    return true;
+  }
+
+  return false;
+}
+
+function validateRegex(value: string, regex: RegExp) {
+  return value.match(regex);
+}
+
+async function createDriverAccount(
+  input: any,
+  res: Response,
+  connection: any,
+  id: any
+) {
+  if (!validateRegex(input.carPlate, /[A-Z]{3}[0-9]{4}/)) {
+    returnErrorStatusWithMessage(res, -5);
+    return;
+  }
+  await connection.query(
+    "insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)",
+    [
+      id,
+      input.name,
+      input.email,
+      input.cpf,
+      input.carPlate,
+      !!input.isPassenger,
+      !!input.isDriver,
+      input.password,
+    ]
+  );
+
+  const obj = {
+    accountId: id,
+  };
+  return obj;
+}
+
+async function createPassengerAccount(input: any, connection: any, id: any) {
+  await connection.query(
+    "insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)",
+    [
+      id,
+      input.name,
+      input.email,
+      input.cpf,
+      null,
+      !!input.isPassenger,
+      !!input.isDriver,
+      input.password,
+    ]
+  );
+  const obj = {
+    accountId: id,
+  };
+  return obj;
+}
+
 app.post("/signup", async function (req, res) {
-	const input = req.body;
-	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		const id = crypto.randomUUID();
-		let result;
-		const [acc] = await connection.query("select * from ccca.account where email = $1", [input.email]);
-		if (!acc) {
+  const input = req.body;
+  const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+  try {
+    const id = crypto.randomUUID();
+    let result;
+    const [acc] = await connection.query(
+      "select * from ccca.account where email = $1",
+      [input.email]
+    );
 
-			if (input.name.match(/[a-zA-Z] [a-zA-Z]+/)) {
-				if (input.email.match(/^(.+)@(.+)$/)) {
+    if (userAlreadyExists(acc, res)) return;
 
-					if (validateCpf(input.cpf)) {
-						if (input.isDriver) {
-							if (input.carPlate.match(/[A-Z]{3}[0-9]{4}/)) {
-								await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
-								
-								const obj = {
-									accountId: id
-								};
-								result = obj;
-							} else {
-								// invalid car plate
-								result = -5;
-							}
-						} else {
-							await connection.query("insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)", [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
+    if (!validateRegex(input.name, /[a-zA-Z] [a-zA-Z]+/)) {
+      returnErrorStatusWithMessage(res, -3);
+      return;
+    }
 
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						}
-					} else {
-						// invalid cpf
-						result = -1;
-					}
-				} else {
-					// invalid email
-					result = -2;
-				}
+    if (!validateRegex(input.email, /^(.+)@(.+)$/)) {
+      returnErrorStatusWithMessage(res, -2);
+      return;
+    }
 
-			} else {
-				// invalid name
-				result = -3;
-			}
+    if (!validateCpf(input.cpf)) {
+      returnErrorStatusWithMessage(res, -1);
+      return;
+    }
 
-		} else {
-			// already exists
-			result = -4;
-		}
-		if (typeof result === "number") {
-			res.status(422).json({ message: result });
-		} else {
-			res.json(result);
-		}
-	} finally {
-		await connection.$pool.end();
-	}
+    if (input.isDriver) {
+      result = await createDriverAccount(input, res, connection, id);
+    } else {
+      result = await createPassengerAccount(input, connection, id);
+    }
+
+    if (typeof result === "number") {
+      res.status(422).json({ message: result });
+    } else {
+      res.json(result);
+    }
+  } finally {
+    await connection.$pool.end();
+  }
+});
+
+app.get("/accounts/:accountId", async function (req, res) {
+  const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
+  const [accountData] = await connection.query(
+    "select * from ccca.account where account_id = $1",
+    [req.params.accountId]
+  );
+  await connection.$pool.end();
+  res.json(accountData);
 });
 
 app.listen(3000);
---
+++ test/signup.test.ts
@@ -0,0 +1,158 @@
+import axios from "axios";
+
+axios.defaults.validateStatus = function () {
+  return true;
+};
+
+describe("signup", () => {
+  test("should create a new passenger account", async () => {
+    const input = {
+      name: "john doe",
+      email: `john.doe${Math.random()}@gmail.com`,
+      cpf: "41121955088",
+      isPassenger: true,
+      password: "0293490239043",
+    };
+
+    const responseSignup = await axios.post(
+      "http://localhost:3000/signup",
+      input
+    );
+    const outputSignup = responseSignup.data;
+    expect(outputSignup.accountId).toBeDefined();
+    const responseGetAccount = await axios.get(
+      `http://localhost:3000/accounts/${outputSignup.accountId}`
+    );
+    const outputGetAccount = responseGetAccount.data;
+    expect(outputGetAccount).toEqual({
+      account_id: outputSignup.accountId,
+      name: input.name,
+      email: input.email,
+      cpf: input.cpf,
+      car_plate: null,
+      is_passenger: true,
+      is_driver: false,
+      password: input.password,
+    });
+  });
+
+  test("should not create a new passenger account with invalid name", async () => {
+    const input = {
+      name: "john",
+      email: `john.doe${Math.random()}@gmail.com`,
+      cpf: "41121955088",
+      isPassenger: true,
+      password: "0293490239043",
+    };
+
+    const responseSignup = await axios.post(
+      "http://localhost:3000/signup",
+      input
+    );
+    expect(responseSignup.status).toBe(422);
+    expect(responseSignup.data).toEqual({ message: -3 });
+  });
+
+  test("should not create a new passenger account with invalid email", async () => {
+    const input = {
+      name: "john Doe",
+      email: `john.doe${Math.random()}@`,
+      cpf: "41121955088",
+      isPassenger: true,
+      password: "0293490239043",
+    };
+
+    const responseSignup = await axios.post(
+      "http://localhost:3000/signup",
+      input
+    );
+    expect(responseSignup.status).toBe(422);
+    expect(responseSignup.data).toEqual({ message: -2 });
+  });
+
+  test("should not create a new passenger account with invalid CPF", async () => {
+    const input = {
+      name: "john Doe",
+      email: `john.doe${Math.random()}@gmail.com`,
+      cpf: "12934",
+      isPassenger: true,
+      password: "0293490239043",
+    };
+
+    const responseSignup = await axios.post(
+      "http://localhost:3000/signup",
+      input
+    );
+    expect(responseSignup.status).toBe(422);
+    expect(responseSignup.data).toEqual({ message: -1 });
+  });
+
+  test("should not create a new passenger when user already exists", async () => {
+    const input = {
+      name: "john Doe",
+      email: `john.doe@gmail.com`,
+      cpf: "41121955088",
+      isPassenger: true,
+      password: "0293490239043",
+    };
+
+    const responseSignup = await axios.post(
+      "http://localhost:3000/signup",
+      input
+    );
+    expect(responseSignup.status).toBe(422);
+    expect(responseSignup.data).toEqual({ message: -4 });
+  });
+
+  test("should create a new driver account", async () => {
+    const input = {
+      name: "john Doe",
+      email: `john.doe${Math.random()}@gmail.com`,
+      cpf: "41121955088",
+      isPassenger: false,
+      isDriver: true,
+      carPlate: "ABC1234",
+      password: "0293490239043",
+    };
+
+    const responseSignup = await axios.post(
+      "http://localhost:3000/signup",
+      input
+    );
+    const outputSignup = responseSignup.data;
+    expect(outputSignup.accountId).toBeDefined();
+    const responseGetAccount = await axios.get(
+      `http://localhost:3000/accounts/${outputSignup.accountId}`
+    );
+    const outputGetAccount = responseGetAccount.data;
+    expect(outputGetAccount).toEqual({
+      account_id: outputSignup.accountId,
+      name: input.name,
+      email: input.email,
+      cpf: input.cpf,
+      car_plate: "ABC1234",
+      is_passenger: false,
+      is_driver: true,
+      password: input.password,
+    });
+  });
+
+  test("should not create a new driver account with invalid carplate", async () => {
+    const input = {
+      name: "john Doe",
+      email: `john.doe${Math.random()}@gmail.com`,
+      cpf: "41121955088",
+      isPassenger: false,
+      isDriver: true,
+      carPlate: "12883",
+      password: "0293490239043",
+    };
+
+    const responseSignup = await axios.post(
+      "http://localhost:3000/signup",
+      input
+    );
+    expect(responseSignup.status).toBe(422);
+    expect(responseSignup.data).toEqual({ message: -5 });
+  });
+});
